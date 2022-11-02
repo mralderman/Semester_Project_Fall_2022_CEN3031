10d5e8a169a12cec7ccc9528808f61df58ed0a5d
mralderman <aldermanza@gmail.com>
2022-11-01 20:07:15 -0400

    updated files for testing and data.py added read an write function from file. no checks have been implemeted

--- data.py
+++ data.py
@@ -10,9 +10,13 @@ class User:
     def get_user_pas(self):
         return self.pas
 
-    def add_activity(self,name, rate, amount):
+    def add_activity(self, user_id, name, rate, amount):
         self.activities.append(Activity(name, rate, amount))
-
+        with open('data.csv', 'a', newline='') as file:
+            append_object = csv.writer(file)
+            appender = [user_id,name,rate,amount]
+            append_object.writerow(appender)
+        file.close()
 
 class Activity:
     def __init__(self, name, rate, amount):
@@ -27,12 +31,51 @@ users = dict({'test': User("test", 1)})
 
 def new_user(user_id, pas, key):
     users[user_id] = User(pas, key)
+    with open('user_pass.csv', 'a', newline='') as file:
+        append_object = csv.writer(file)
+        appender = [user_id,pas,key]
+        append_object.writerow(appender)
+    file.close()
+
+def get_users_from_file(): # use this function first to make the dictionary of user names
+    
+    with open('user_pass.csv') as file:
+        reader = csv.reader(file, delimiter=',')
+        next(reader)
+        for row in reader:
+            users.update({row[0] : User(row[1], row[2])})
+    file.close()
+
+def get_activities_from_file():
+    
+    with open('data.csv') as file:
+        reader = csv.reader(file, delimiter=',')
+        next(reader)
+        for row in reader:
+            users[row[0]].activities.append(row[1],float(row[2]),float(row[3]))
+    file.close()
+
 
 # Please Fix
 # CSV should be as follows:
 # num of users
 # list users' user_ids, pas, key
 # list of activites: user_id, name (of activity), rate, amount
+
+# things I need to make
+# 1. write data to file x
+# 2. read from file and add to user's activity's list at the beginning x
+# 3. read from users file and create a list of users x
+
+get_users_from_file()
+
+for i in users:
+    print(i, " ", users[i].pas)
+
+
+
+
+
 
 """
 def check_similar_users(users, value):
